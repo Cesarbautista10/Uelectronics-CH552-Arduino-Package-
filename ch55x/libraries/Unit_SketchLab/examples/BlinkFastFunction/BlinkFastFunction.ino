a2697ae3b5c5b903ca75b6f28cad15cd46f55fcf
Cesar Bautista <cesar.bautista@uelectronics.com>
2024-03-28 17:25:08 -0600

    task: create a new sketch

--- ch55x/libraries/Unit_SketchLab/examples/BlinkFastFunction/BlinkFastFunction.ino
+++ ch55x/libraries/Unit_SketchLab/examples/BlinkFastFunction/BlinkFastFunction.ino
@@ -6,6 +6,10 @@
   Most Arduinos have an on-board LED you can control. On the simpleCH552
   it is attached to digital pin P3.3
 
+  this example uses marco-based function to accelerate IO access speed. 
+  Note these functions will not disable analog output(PWM)
+  And they only accept constants.
+
   If you want to know what pin the on-board LED is connected to on your Arduino
   model, check the Technical Specs of your board at:
   https://www.arduino.cc/en/Main/Products
@@ -19,25 +23,28 @@
   modified 13 Jun 2020
   by Deqing Sun for use with CH55xduino
   modified 28 Mar 2024
-  by Cesar Bautista
-
+  by Cesar Bautista for use Unit_SketchLab
   This example code is in the public domain.
 
   http://www.arduino.cc/en/Tutorial/Blink
 */
 
-#define LED_BUILTIN 34
+//P33
+#define LED_BUILTIN_PORT 3
+#define LED_BUILTIN_PIN 3
+
+
 
 // the setup function runs once when you press reset or power the board
 void setup() {
   // initialize digital pin LED_BUILTIN as an output.
-  pinMode(LED_BUILTIN, OUTPUT);
+  pinModeFast(LED_BUILTIN_PORT,LED_BUILTIN_PIN, OUTPUT);
 }
 
 // the loop function runs over and over again forever
 void loop() {
-  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
+  digitalWriteFast(LED_BUILTIN_PORT,LED_BUILTIN_PIN, HIGH);   // turn the LED on (HIGH is the voltage level)
   delay(1000);                       // wait for a second
-  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
+  digitalWriteFast(LED_BUILTIN_PORT,LED_BUILTIN_PIN, LOW);    // turn the LED off by making the voltage LOW
   delay(1000);                       // wait for a second
 }
